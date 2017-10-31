670b2ae2f1a3a514eb45fb528a23543587f65aba
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-31 11:48:40 -0700

    Add name replacement in text.

--- src/app/page.model.ts
+++ src/app/page.model.ts
@@ -2,4 +2,8 @@ import { Option } from './option.model';
 
 export class Page {
   constructor (public text: string, public options: Option[], public id: number) {}
+
+  displayText(name: string) {
+    return this.text.replace(/\[name\]/g, name);
+  }
 }
--- src/app/pages.ts
+++ src/app/pages.ts
@@ -32,24 +32,24 @@ export const PAGES: Page[] = [
   new Page(
     "Choosing to be the SIDEKICK in your own adventure? That's an... interesting decision. Are you certain you want to proceed down this path?",
     [
-      new Option("Go to second page", 2, -1),
-      new Option("Go to first Page", 1, -2)
+      new Option("No, I've changed my mind.", 1, -20),
+      new Option("Yes, I'm ready to support my SUPERHERO.", 9, 0)
     ],
     4
   ),
   new Page(
     "The ROMANTIC INTEREST? You must be very fond of beind held hostage. If not, you may wish to choose a different path. Are you sure you wish to proceed?",
     [
-      new Option("Go to second page", 2, -1),
-      new Option("Go to first Page", 1, -2)
+      new Option("No, I like having agency.", 1, -10),
+      new Option("Yes, bring on the kidnappings.", 8, 0)
     ],
     5
   ),
   new Page(
-    "I think you've made a mistake. You do realize what the fate of an ORDINARY CITIZEN is likely to be in a SUPERHERO COMIC, yes? It's NOT GOOD. You really ought to choose someone PLOT-CRITICAL.",
+    "I think you've made a mistake. You do realize what the fate of an ORDINARY CITIZEN is likely to be in a SUPERHERO COMIC, yes? It's NOT GOOD. You really ought to choose someone PLOT-CRITICAL. Why don't you do that now?",
     [
-      new Option("Go to second page", 2, -1),
-      new Option("Go to first Page", 1, -2)
+      new Option("Yes, I want to live.", 1, -1),
+      new Option("No, I understand the risks.", 7, 0)
     ],
     6
   )
